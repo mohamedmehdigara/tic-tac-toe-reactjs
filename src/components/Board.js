f2cedc051592cfbfefc17b0cdee4f49a126f8356
mohamedmehdigara <garamehdi57@yahoo.com>
2023-05-01 07:38:39 +0100

    added files

---
+++ src/components/Board.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Cell from "./Cell";
+
+function Board() {
+ 
+  return (
+      <div className="board">
+          <Cell row={0} column={0} />
+          <Cell row={0} column={1} />
+          <Cell row={0} column={2} />
+         
+          <Cell row={1} column={0} />
+          <Cell row={1} column={1} />
+          <Cell row={1} column={2} />
+
+          <Cell row={2} column={0} />
+          <Cell row={2} column={1} />
+          <Cell row={2} column={2} />
+     </div>
+  )
+}
+
+export default Board
